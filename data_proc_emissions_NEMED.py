53958e9b53cbfe95b0e39149e65b3c2013856c26
David Saldivia <david.saldivia.s@gmail.com>
2024-01-09 04:55:42 +1100

    updating files

--- data_proc_emissions_NEMED.py
+++ data_proc_emissions_NEMED.py
@@ -5,39 +5,16 @@ Created on Thu Dec  7 08:16:57 2023
 @author: z5158936
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 14 18:15:34 2023
-
-@author: z5158936
-"""
-
 import nemed
 from nemed.downloader import download_aemo_cdeii_summary, download_unit_dispatch
 from nemed.process import aggregate_data_by
 
 # To generate plots shown 
 import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
-from plotly.subplots import make_subplots
-
-# Open plot in browser (optional)
-import plotly.io as pio
-# pio.renderers.default = "browser"
-
-import time                 # to measure the computation time
 import os
 import datetime
-import sys 
-
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
-from scipy import stats
-import tm_solarshift.trnsys_utils as TRP
 
 fileDir = os.path.dirname(os.path.abspath(__file__))
 emissionsDir = os.path.join(fileDir,"data","emissions")
@@ -53,15 +30,19 @@ for (type_index, year) in [
         ]:
     #### Plotting a sample of Intensity_Index
     if type_index == "total":
-        file_emissions = os.path.join(emissionsDir, f'emissions_year_{year}_total.csv')
+        file_emissions = os.path.join(
+            emissionsDir,
+            f'emissions_year_{year}_total.csv'
+            )
         if not(os.path.isfile(file_emissions)):
-            emissions = nemed.get_total_emissions(start_time=f"{year}/01/01 00:00",
-                                                end_time=f"{year}/12/31 23:59",
-                                                cache=CacheDir,
-                                                filter_regions=['NSW1','VIC1','SA1','QLD1'])
-            
+            emissions = nemed.get_total_emissions(
+                start_time=f"{year}/01/01 00:00",
+                end_time=f"{year}/12/31 23:59",
+                cache=CacheDir,
+                filter_regions=['NSW1','VIC1','SA1','QLD1']
+            )
             emissions.index = pd.to_datetime(emissions.TimeEnding)
-            emissions.drop(columns=['TimeEnding'],inplace=True)
+            emissions.drop(columns=['TimeEnding'], inplace=True)
             emissions.to_csv( file_emissions )
         else:
             emissions = pd.read_csv(file_emissions,index_col=0)
@@ -69,13 +50,16 @@ for (type_index, year) in [
     
     
     if type_index == "marginal":
-        file_emissions = os.path.join(emissionsDir, f'emissions_year_{year}_marginal.csv')
+        file_emissions = os.path.join(
+            emissionsDir,
+            f'emissions_year_{year}_marginal.csv'
+            )
         if not(os.path.isfile(file_emissions)):
-            emissions = nemed.get_marginal_emissions(start_time=f"{year}/01/01 00:00",
-                                                end_time=f"{year}/12/31 23:59",
-                                                cache=CacheDir,
-                                                )
-            
+            emissions = nemed.get_marginal_emissions(
+                start_time=f"{year}/01/01 00:00",
+                end_time=f"{year}/12/31 23:59",
+                cache=CacheDir,
+                )
             emissions.index = pd.to_datetime(emissions.Time)
             emissions.drop(columns=['Time'],inplace=True)
             emissions.rename(
