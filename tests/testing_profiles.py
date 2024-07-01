fd1b003bdcc5debf0d28a7608d6984fe56be9b31
David Saldivia <david.saldivia.s@gmail.com>
2024-07-02 08:25:22 +1000

    testing

--- tests/testing_profiles.py
+++
@@ -1,6 +0,0 @@
-import tm_solarshift.utils.profiles as profiles
-import tm_solarshift.utils.trnsys as trnsys
-
-Sim = trnsys.Simulation()
-Profiles = profiles.new_profile(Sim)
-print(Profiles.head())
